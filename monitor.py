238f393a063946ecb513dbe8dfbfa266fcfff38e
Geon1994 <ngg1261@naver.com>
2026-04-16 20:08:43 +0900

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -17,10 +17,11 @@ def send_telegram(msg):
     )
 
 options = Options()
-options.add_argument("--headless")
+options.add_argument("--headless=new")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-gpu")
+options.add_argument("--window-size=1920,1080")
 
 driver = webdriver.Chrome(
     service=Service(ChromeDriverManager().install()),
